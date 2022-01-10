a0e66900aac136756d369670b3d3b8b15d011bbb
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2022-01-10 23:54:34 +0600

    Rest of the Cart Page design

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -39,9 +39,98 @@ const Cart = () => {
               <span className={styles.total}>439.80</span>
             </td>
           </tr>
+          <tr>
+            <td className={styles.imgContainer}>
+              <Image
+                src='/img/pizza.png'
+                layout='fill'
+                objectFit='cover'
+                alt='pizza'
+              />
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td className={styles.price}>$19.99</td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>439.80</span>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.imgContainer}>
+              <Image
+                src='/img/pizza.png'
+                layout='fill'
+                objectFit='cover'
+                alt='pizza'
+              />
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td className={styles.price}>$19.99</td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>439.80</span>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.imgContainer}>
+              <Image
+                src='/img/pizza.png'
+                layout='fill'
+                objectFit='cover'
+                alt='pizza'
+              />
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td className={styles.price}>$19.99</td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>439.80</span>
+            </td>
+          </tr>
         </table>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$87.98
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$87.98
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$87.98
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
+        </div>
+      </div>
     </div>
   );
 };
